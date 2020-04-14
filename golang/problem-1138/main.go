676bd7c4bec2aba3174c00e9a2992e511831fd47
Sean Cheng <blackhorseya@gmail.com>
2020-04-14 17:08:03 +0800

    [golang] refactor: problem 1138

--- golang/problem-1138/main.go
+++ golang/problem-1138/main.go
@@ -1,40 +1,54 @@
 package main
 
-import (
-	"fmt"
-)
+// Define a coordinate structure
+type Point struct {
+	X int32
+	Y int32
+}
+
+// Get pointer of Point by letter
+func NewPointByLetter(letter int32) *Point {
+	pos := letter - 'a'
+	return &Point{
+		X: pos % 5,
+		Y: pos / 5,
+	}
+}
+
+// Get the path of "from" to "to" in the fourth quadrant
+func (from *Point) WalkTo(to *Point) string {
+	var result = ""
+
+	for from.Y > to.Y {
+		result += "U"
+		from.Y--
+	}
+	
+	for from.X > to.X {
+		result += "L"
+		from.X--
+	}
+	
+	for from.Y < to.Y {
+		result += "D"
+		from.Y++
+	}
+	
+	for from.X < to.X {
+		result += "R"
+		from.X++
+	}
+
+	return result + "!"
+}
 
 func Solve(target string) string {
 	var result = ""
 
-	var flagRow, flagCol int
+	var current = &Point{}
 	for _, letter := range target {
-		pos := int(letter) - int('a')
-		row := pos / 5
-		col := pos % 5
-
-		for flagRow > row {
-			result += "U"
-			flagRow--
-		}
-
-		for flagCol > col {
-			result += "L"
-			flagCol--
-		}
-
-		for flagRow < row {
-			result += "D"
-			flagRow++
-		}
-
-		for flagCol < col {
-			result += "R"
-			flagCol++
-		}
-
-		result += "!"
-		fmt.Println(pos, row, col, result)
+		goal := NewPointByLetter(letter)
+		result += current.WalkTo(goal)
 	}
 
 	return result
--- golang/problem-1138/main_test.go
+++ golang/problem-1138/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -32,6 +33,105 @@ func TestSolve(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Solve(tt.args.target); got != tt.want {
 				t.Errorf("Solve() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewPointByLetter(t *testing.T) {
+	type args struct {
+		letter int32
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Point
+	}{
+		{
+			name: "l_return_{1,2}",
+			args: args{
+				letter: 'l',
+			},
+			want: &Point{
+				X: 1,
+				Y: 2,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewPointByLetter(tt.args.letter); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPointByLetter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPoint_WalkTo(t *testing.T) {
+	type fields struct {
+		X int32
+		Y int32
+	}
+	type args struct {
+		goal *Point
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name:   "{0,0}_{1,2}_return_DDR!",
+			fields: fields{
+				X: 0,
+				Y: 0,
+			},
+			args:   args{
+				goal: &Point{
+					X: 1,
+					Y: 2,
+				},
+			},
+			want:   "DDR!",
+		},
+		{
+			name:   "{1,2}_{4,0}_return_UURRR!",
+			fields: fields{
+				X: 1,
+				Y: 2,
+			},
+			args:   args{
+				goal: &Point{
+					X: 4,
+					Y: 0,
+				},
+			},
+			want:   "UURRR!",
+		},
+		{
+			name:   "{4,0}_{4,0}_return_!",
+			fields: fields{
+				X: 4,
+				Y: 0,
+			},
+			args:   args{
+				goal: &Point{
+					X: 4,
+					Y: 0,
+				},
+			},
+			want:   "!",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Point{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+			}
+			if got := p.WalkTo(tt.args.goal); got != tt.want {
+				t.Errorf("WalkTo() = %v, want %v", got, tt.want)
 			}
 		})
 	}
