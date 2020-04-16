5b03553a76cfa5f607aed7ec54b7ad04da18ed89
Sean Cheng <blackhorseya@gmail.com>
2020-04-16 19:16:26 +0800

    [golang] feat: not implement problem 160

---
+++ golang/problem-160/main.go
@@ -0,0 +1,15 @@
+package main
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func Solve(headA, headB *ListNode) *ListNode {
+	// todo: 2020-04-16|19:15|doggy|implement it
+	return nil
+}
+
+func main() {
+	
+}
