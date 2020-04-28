c2402cbcae12af3f76ac67575edb236f30b46eef
Sean Cheng <blackhorseya@gmail.com>
2020-04-28 19:09:09 +0800

    [golang] refactor: problem 88
    - refactor logic

--- golang/problem-88/main.go
+++ golang/problem-88/main.go
@@ -1,12 +1,37 @@
 package main
 
-import (
-	"sort"
-)
-
 func Solve(nums1 []int, m int, nums2 []int, n int) {
-	nums1 = append(nums1[:m], nums2[:n]...)
-	sort.Ints(nums1)
+	var (
+		merged = make([]int, len(nums1))
+		i      = 0
+		j      = 0
+		k      = 0
+	)
+	for i < m && j < n {
+		if nums1[i] <= nums2[j] {
+			merged[k] = nums1[i]
+			i++
+		} else {
+			merged[k] = nums2[j]
+			j++
+		}
+
+		k++
+	}
+
+	for i < m {
+		merged[k] = nums1[i]
+		i++
+		k++
+	}
+
+	for j < n {
+		merged[k] = nums2[j]
+		j++
+		k++
+	}
+
+	copy(nums1, merged)
 }
 
 func main() {
