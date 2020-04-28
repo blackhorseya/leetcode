528c289ba82c59a6a18fec67e3054ecebb8c6a15
Sean Cheng <blackhorseya@gmail.com>
2020-04-28 18:59:14 +0800

    [golang] fix: problem 88
    - method return type
    - test case

--- golang/problem-88/main.go
+++ golang/problem-88/main.go
@@ -4,10 +4,9 @@ import (
 	"sort"
 )
 
-func Solve(nums1 []int, m int, nums2 []int, n int) []int {
+func Solve(nums1 []int, m int, nums2 []int, n int) {
 	nums1 = append(nums1[:m], nums2[:n]...)
 	sort.Ints(nums1)
-	return nums1
 }
 
 func main() {
--- golang/problem-88/main_test.go
+++ golang/problem-88/main_test.go
@@ -30,8 +30,9 @@ func TestSolve(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Solve(tt.args.nums1, tt.args.m, tt.args.nums2, tt.args.n); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Solve() = %v, want %v", got, tt.want)
+			Solve(tt.args.nums1, tt.args.m, tt.args.nums2, tt.args.n)
+			if !reflect.DeepEqual(tt.args.nums1, tt.want) {
+				t.Errorf("Solve() = %v, want %v", tt.args.nums1, tt.want)
 			}
 		})
 	}
