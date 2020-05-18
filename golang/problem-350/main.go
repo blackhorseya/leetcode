339361460b59bd0543e3e4da6f636d5fb31251e7
Sean Cheng <blackhorseya@gmail.com>
2020-05-18 18:58:04 +0800

    [golang] refactor: problem 350

--- golang/problem-350/main.go
+++ golang/problem-350/main.go
@@ -7,27 +7,22 @@ func Solve(nums1 []int, nums2 []int) []int {
 		return Solve(nums2, nums1)
 	}
 
+	maps := make(map[int]int)
+	for _, num := range nums1 {
+		maps[num] += 1
+	}
+
 	var ret []int
 	for _, num := range nums2 {
-		contain, pos := contain(nums1, num)
-		if contain {
+		count, ok := maps[num]
+		if ok && count != 0 {
 			ret = append(ret, num)
-			nums1 = append(nums1[:pos], nums1[pos+1:]...)
+			maps[num] -= 1
 		}
 	}
 
 	sort.Ints(ret)
 	return ret
-}
-
-func contain(nums []int, target int) (contain bool, pos int) {
-	for index, num := range nums {
-		if num == target {
-			return true, index
-		}
-	}
-
-	return false, 0
 }
 
 func main() {
