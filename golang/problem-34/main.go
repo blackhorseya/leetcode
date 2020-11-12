aae0c95cf6074d5dd06577b4901d59dbad4fbc13
Sean Cheng <blackhorseya@gmail.com>
2020-11-12 17:55:59 +0800

    [golang] refactor: problem-34 coding style

--- golang/problem-34/main.go
+++ golang/problem-34/main.go
@@ -1,24 +1,14 @@
 package main
 
 func Solve(nums []int, target int) []int {
-	if len(nums) == 0 {
-		return []int{-1, -1}
-	}
-
 	ret := []int{-1, -1}
-	for i, num := range nums {
-		if num == target {
+	for i := 0; i < len(nums) && nums[i] <= target; i++ {
+		if nums[i] == target {
 			if ret[0] == -1 {
 				ret[0] = i
 			}
 
-			if ret[1] < i {
-				ret[1] = i
-			}
-		}
-
-		if num != target && ret[0] != -1 {
-			break
+			ret[1] = i
 		}
 	}
 
